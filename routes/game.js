a4eda2147e029c249ba709dd10ce2615706813e2
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-08-01 11:01:55 -0600

    leaderboard working

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const questionController = require("../controllers/questionController");
+const leaderBoardController = require("../controllers/leaderBoardController");
 
 router.get("/play", async function (req, res, next) {
   // TODO: Implement Game
@@ -9,7 +10,7 @@ router.get("/play", async function (req, res, next) {
   res.render("play", { user: req.session.user, questions: results });
 });
 
-router.post("/gameFinished", function (req, res, next) {
+router.post("/gameFinished", async function (req, res, next) {
   // implement game
   //data from the form submision
   let answers = req.body;
@@ -17,23 +18,32 @@ router.post("/gameFinished", function (req, res, next) {
   //score for the game
   let score = 0;
   //setup questions into an answersArr
-  for(var i in answers){
-    if(i === "correct_answer"){
-      console.log('crrect')
-    }else{
-      answersArr.push(answers [i])
+  for (var i in answers) {
+    if (i === "correct_answer") {
+      console.log("crrect");
+    } else {
+      answersArr.push(answers[i]);
     }
   }
 
   //loop through questions to check which ones are right
   for (let i = 0; i < answers.correct_answer.length; i++) {
     if (answers.correct_answer[i] == answersArr[i]) {
-      console.log("correct_answer");
+      // console.log("correct_answer");
       score += 1;
     }
   }
 
-  res.render("finishedGame", { user: req.session.user, score: score });
+  //add users score to db
+  let user = req.session.user;
+  console.log(user)
+  let result = await leaderBoardController.addUsersScore(user.username, score);
+
+  res.render("finishedGame", {
+    user: req.session.user,
+    score: score,
+    result: result,
+  });
 });
 
 module.exports = router;
