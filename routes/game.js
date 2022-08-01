518e21b4b291464a137b4b46d92c04a6c4a37573
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-08-01 09:05:07 -0600

    gamefinished shows correct answers

--- routes/game.js
+++ routes/game.js
@@ -11,10 +11,30 @@ router.get("/play", async function (req, res, next) {
 
 router.post("/gameFinished", function (req, res, next) {
   // implement game
+  //data from the form submision
   let answers = req.body;
-  console.log(answers);
+  let answersArr = [];
+  //score for the game
+  let score = 0;
+  //setup questions into an answersArr
+  for(var i in answers){
+    if(i === "correct_answer"){
+      console.log('crrect')
+    }else{
+      answersArr.push(answers [i])
+    }
+  }
 
-  res.render("finishedGame", { user: req.session.user });
+
+  //loop through questions to check which ones are right
+  for (let i = 0; i < answers.correct_answer.length; i++) {
+    if (answers.correct_answer[i] == answersArr[i]) {
+      console.log("correct_answer");
+      score += 1;
+    }
+  }
+
+  res.render("finishedGame", { user: req.session.user, score: score });
 });
 
 module.exports = router;
