fb5953737c0798792d46a657696987a5ff324daf
PhillipDuarte1 <60678196+PhillipDuarte1@users.noreply.github.com>
2022-08-12 10:18:32 -0600

    an admin can now update the pending question to be approved or dendied to the mongodb

--- data/mongoDAL.js
+++ data/mongoDAL.js
@@ -1,6 +1,6 @@
 // mongoDAL is responsible to for all interactions with mongodb for the trivia game
 const e = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const Result = require('../models/result').Result;
 const STATUS_CODES = require('../models/statusCodes').STATUS_CODES;
 
@@ -10,6 +10,7 @@ const client = new MongoClient(URL);
 const dbName = "Web-Security-Questions";
 const db = client.db(dbName);
 const pendingCollection = db.collection("PendingQuestions");
+const triviaCollection = db.collection("Questions");
 
 exports.getAllQuestions = async (collection) => {
   let result;
@@ -24,6 +25,19 @@ exports.getAllQuestions = async (collection) => {
   return result;
 };
 
+exports.getOneQuestion = async (collection, id) => {
+  let result;
+  const question = db.collection(`${collection}`);
+  client.connect();
+  try {
+    result = await question.find({_id: ObjectId(`${id}`)}).toArray();
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+  return result;
+}
+
 exports.addTrivia = async (trivia) => {
   let result;
   client.connect();
@@ -32,7 +46,7 @@ exports.addTrivia = async (trivia) => {
       return new Result(STATUS_CODES.failure, 'Trivia Format Invalid');
     } else {
       result = await pendingCollection.insertOne(trivia, (err, res) => {
-        if(err) {
+        if (err) {
           console.log(err);
           throw err;
         }
@@ -46,6 +60,24 @@ exports.addTrivia = async (trivia) => {
   }
 }
 
-exports.denyTrivia = async (trivia) => {
-  console.log('denied');
+exports.updateApprovalOfQuestion = async (triviaObj) => {
+  let result;
+  client.connect();
+  try {
+    if (triviaObj == undefined) {
+      return new Result(STATUS_CODES.failure, 'Trivia Format Invalid');
+    } else {
+      result = await triviaCollection.insertOne(triviaObj, (err, res) => {
+        if (err) {
+          console.log(err);
+          throw err;
+        }
+        client.close();
+      });
+      return new Result(STATUS_CODES.success, 'Valid Trivia Object', triviaObj)
+    }
+  } catch (err) {
+    console.log(err);
+    return result;
+  }
 }
